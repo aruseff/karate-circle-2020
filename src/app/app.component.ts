19363e98bbb4d2584d54cd174c4374f228cdfe89
Andrey Rusev <arusev@vmware.com>
2020-06-09 14:39:22 +0300

    UI updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'karate-circle2020';
+
+  signalsForLast: SelectItem[] = [
+    { label: '20s', value: '20s' },
+    { label: '10s', value: '10s' },
+    { label: '5s', value: '5s' },
+  ];
+
+  selectedSignalsForLast = [];
+
+  value3;
+  val;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+// PrimeNg Modules
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MainPanelComponent } from './components/main-panel/main-panel.component';
+import { TabViewModule } from 'primeng/tabview';
+import { CodeHighlighterModule } from 'primeng/codehighlighter';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainPanelComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    // PrimeNg Modules
+    SelectButtonModule,
+    CardModule,
+    FieldsetModule,
+    InputNumberModule,
+    InputTextModule,
+    TabViewModule,
+    CodeHighlighterModule,
+    ButtonModule,
+    DropdownModule,
+    ProgressBarModule,
+    CheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
