006091c3eccece8f29f5921489ff9089e9eb0153
aruseff <andrey.ruseff@gmail.com>
2024-03-18 23:37:01 +0200

    WIP: clean up

--- main.ts
+++ main.ts
@@ -58,7 +58,6 @@ function createWindow(): BrowserWindow {
 }
 
 try {
-
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
